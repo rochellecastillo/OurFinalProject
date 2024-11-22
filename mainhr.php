e827c44d14a42951cb3497cc898d9865251c5c66
abbygailtalas <talasabbygaileunice@gmail.com>
2024-11-22 13:27:16 +0800

    add employee

--- mainhr.php
+++ mainhr.php
@@ -16,7 +16,82 @@ if(!isset($_SESSION['userid'])){
 </head>
 <body>
     <?php
+    include_once'class/employee.php';
+    $e=new employee();
+    if(isset($_POST['save'])){
+        $idnum=$_POST['idnum'];
+        $ln=$_POST['ln'];
+        $fn=$_POST['fn'];
+        $mn=$_POST['mn'];
+        $address=$_POST['address'];
+        $bdate=$_POST['bdate'];
+        $gender=$_POST['gender'];
+        $contact=$_POST['contact'];
+        echo'
+            <script>
+                Swal.fire({
+				title: "Success",
+				text: "'.$e->addemployee($idnum,$ln,$fn,$mn,$address,$bdate,$gender,$contact,'employee').'",
+				icon: "success"
+				});
+            </script>
+        
+        ';
+        
+    }
     include_once'res/nav.php';
     ?>
+    <div class="container">
+        <form method="POST">
+            <div class="row mt-3">
+                <div class="col-md-3">
+                    <label for="idnum">Id Number</label>
+                    <input type="text" class="form-control" name="idnum">
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-md-4">
+                    <label for="ln">Last Name</label>
+                    <input type="text" class="form-control" name="ln">
+                </div>
+                <div class="col-md-4">
+                    <label for="fn">First Name</label>
+                    <input type="text" class="form-control" name="fn">
+                </div>
+                <div class="col-md-4">
+                    <label for="mn">Middle Name</label>
+                    <input type="text" class="form-control" name="mn">
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-md-10">
+                    <label for="address">Address</label>
+                    <input type="text" class="form-control" name="address">
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-md-3">
+                    <label for="bdate">Birth Date</label>
+                    <input type="date" class="form-control" name="bdate">
+                </div>
+                <div class="col-md-3">
+                    <label for="gender">Gender</label>
+                    <select name="gender" id="" class="form-control">
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                    </select>
+                </div>
+                <div class="col-md-3">
+                    <label for="contact">Contact Number</label>
+                    <input type="text" class="form-control" name="contact">
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-md-2">
+                    <button class="btn btn-primary form-control" name="save">SAVE</button>
+                </div>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
